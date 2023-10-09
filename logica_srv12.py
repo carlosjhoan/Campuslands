f3d9193918d22aa8c8181d8453b603d7a8445397
LaliToy <carlosjhoanaguilar@gmail.com>
2023-10-09 18:33:35 -0500

    Me falta poco

--- logica_srv12.py
+++ logica_srv12.py
@@ -110,6 +110,22 @@ while True:
                         else:
                             print(f" {a[0]}\t|\t{a[1]}")
                             print ("=" * 35)
+                    seguir_jugando = input("""\n¿Desea seguir jugando?
+                                            s -- Sí, quiero intentarlo de nuevo
+                                            n -- No, ya me aburrí
+                                            
+                                            >>> ¿Qué desea? """)
+                    
+                    if seguir_jugando == "s" or seguir_jugando == "S":
+                        intentos = 0
+              
+                    else:
+
+                        perm_jugar = "n"
+                        print ("\nQué bueno que lo hayas intentado. Esperamso que te hayas divertido")
+                        input("\nPresione cualquier tecla para volver al menú... ")
+
+
                 else:
                     if num_jugador < num_adivinar:
                         print ("\nFALLASTE!!!\nEl número a adivinar es MAYOR")
@@ -159,5 +175,5 @@ while True:
             break
         else:
             input ("\nPresione cualquier tecla para volver al menú... ")
-            pass ("\nPresione cualquier tecla para volver al menú... ")
+            
             pass
