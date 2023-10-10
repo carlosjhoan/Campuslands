77a534aeaab987e8f3f536fbaccca4a8e8dab2d8
carlosjhoan <43355110+carlosjhoan@users.noreply.github.com>
2023-10-10 20:23:36 +0000

    Punto 1 funciona

--- matrices_cuadradas_srv13.py
+++ matrices_cuadradas_srv13.py
@@ -1,1 +1,125 @@
 #Este programa crea e imprime matrices cuadradas que siguen cierto patrón
+
+#Validación del menú
+def menu(msj):
+    while True:
+        try:
+            opc = int(input(msj))
+            if opc < 1 or opc > 9:
+                print ("Debe ser entre 1 y 9.")
+                
+                continue
+            return opc
+        
+        except ValueError:
+            print ("ERROR !!! Debe ingresar un número entero.")
+
+
+#Función que valida la entrada de un número entero que indica el tamaño de la matriz
+def valid_int(msj):
+    while True:
+        try:
+            num = int(input(msj))
+            if num < 0:
+                print ("\nAdivina con números positivos")
+                continue
+            return num
+        
+        except ValueError:
+            print ("\nERROR!!! Debe ingresar un número entero.")
+
+#Función que crea matrices de ceros
+def crear_matrices_ceros (fil, col):
+    m = []
+    for i in range(fil):
+        fila = [0] * col
+        m.append(fila)
+    return m
+
+
+#Función que llena matrices consecutivos por fila. Indicador 1
+def llenar_matriz_1 (mat):
+    n = 0
+    for f in range(len(mat)):
+        for c in range(len(mat[f])):
+            n += 1 
+            mat [f][c] = n 
+    return mat
+
+#Función que llena matrices consecutivos por columna. Indicador 2
+def llenar_matriz_2 (mat):
+    n = 0
+    for c in range(len(mat)):
+        for f in range(len(mat[f])):
+            n += 1 
+            mat [f][c] = n 
+    return mat
+
+        #print (" ")
+#Función que imrpime matrices
+def print_matrices (mat):
+    for f in range(len(mat)):
+        for c in range(len(mat[f])):
+            print (mat[f][c], end = "\t")
+        print (" ")
+
+while True:
+    print ("\t\t", "=" * 20)
+    opc = menu("""\t\t  MATRICES CUADRADAS
+                 ====================
+               1. Números consecutivos en dirección de las filas
+               2. Números consecutivos por columna
+               3. Números consecutivos por fila y se devuelve
+               4. Matriz diagonal.
+               5. Matriz diagonal recíproca.
+               6. Mitad inferior de la matriz con 1, la otra con 0.
+               7. Mitad superior de la matriz con 1, la otra con 0.
+               8. Llenar con A y con B de manera alternada, media matriz.
+               9. SALIR
+               
+               >>> ¿Opción [1 - 9]? """)
+
+
+    if opc == 1:
+        print ("\n", "-" * 60)
+        print ("1. Números consecutivos en dirección de las filas")
+        print ("-" * 60)
+        num_filas = valid_int("\nNúmero de filas de la matriz: ")
+        matriz_1 = crear_matrices_ceros (num_filas, num_filas)
+        matriz_1_llena = llenar_matriz_1 (matriz_1)
+        print_matrices (matriz_1_llena)
+        input ("\nPresione una tecla para volver al menú... ")
+
+    elif opc == 2:
+        pass
+
+    elif opc == 3:
+        pass
+
+    elif opc == 4:
+        pass
+
+    elif opc == 5:
+        pass
+
+    elif opc == 6:
+        pass
+
+    elif opc == 7:
+        pass
+
+    elif opc == 8:
+        pass
+
+    else:
+        si_no = input("""\n¿Está seguro que desea salir? 
+                      s -- Sí, deseo salir!!
+                      n -- No, deseo continuar!!!""")
+        if si_no == "s" or si_no == "S":
+            print ("\nEsperamos haberle sido de utilidad.")
+            input ("\nPresione cualquier tecla para SALIR... ")
+            break
+        else:
+            input ("\nPresione cualquier tecla para volver al menú... ")
+            
+            pass
