cdb69b8e0ce2aa8e0622cda280aec73047fe904d
carlosjhoan <43355110+carlosjhoan@users.noreply.github.com>
2023-10-11 20:41:10 +0000

    Llenado de la matriz con nombre ciudade y enlaces

--- ciudades_ferrocarril_srv13.py
+++ ciudades_ferrocarril_srv13.py
@@ -14,6 +14,22 @@ def menu(msj):
         except ValueError:
             print ("ERROR!!!\nSolo se permite el ingreso de números enteros... ")
 
+
+#validación nombre del producto
+def valid_nombre(msj):
+    while True:
+        try:
+            nombre = input(msj)
+            if not nombre.isalnum():
+                print ("\nEl nombre del producto solo puede contener letras y números.\n\n***INGRESE UN NOMBRE VÁLIDO SEGÚN LAS INDICACIONES ANTERIORES***")
+                continue
+
+            return nombre
+        
+        except Exception as e:
+            print ("\nERROR!!! Se ha producido un error. Inténtelo de nuevo.", e)
+
+
 #Función para crear matrices
 def crear_matrices_ceros (fil, col):
     m = []
@@ -35,22 +51,66 @@ def valid_int(msj):
         try:
             num = int(input(msj))
             if num < 0:
-                print ("\nLa cantidade ciudades debe ser un valor positivo.\n\n***VUELVA A INGRESAR UN VALOR, ESTA VEZ DE MANERA CORRECTA***")
+                print ("\nLa cantidad de ciudades debe ser un valor positivo.\n\n***VUELVA A INGRESAR UN VALOR, ESTA VEZ DE MANERA CORRECTA***")
                 continue
             return num
         
         except ValueError:
             print ("\nERROR!!! Debe ingresar un número entero.")
 
+mat_ciudades = []
+
 while True:
 
-    opc = menu(msj)
+    print ("\t\t********************")
+    opc = menu("""   \t\tRED DE FERROCARRILES
+                ********************
+               1. Llenar ciudades. 
+               3. Verificar ruta directa entre 2 ciudades
+               4. SALIR
+               
+               >>> ¿Opción [1 - 2]? """)
 
     if opc == 1:
-        pass
+        ciudad_list = []
+        ciudad_dicc = {}
+        enlaces = {}
+        print ("\n", "-" * 60)
+        print ("1. Llenar ciudades")
+        print ("-" * 60)
+        num_ciudades = valid_int("\nCantidade de ciudades: ")
+        
+        for i in range(num_ciudades):
+            ciudad_list = []
+            ciudad_dicc = {}
+            enlaces_list = []
+            enlaces = {}
+            nombre_ciudad = valid_nombre(f"\nNombre de la ciudad {i + 1}: ")
+            ciudad_dicc["nombre"] = nombre_ciudad
+            ciudad_list.append(ciudad_dicc)
+
+            num_enlaces = valid_int(f"\n¿Con cuántas ciudades está enlazada {ciudad_dicc['nombre']}? ")
+            for a in range(num_enlaces):
+                nombre_enlace = valid_nombre(f"\nEnlace N° {a + 1}: ")
+                enlaces_list.append(nombre_enlace)
+            enlaces["enlaces"] = enlaces_list
+            ciudad_list.append(enlaces)
+            mat_ciudades.append(ciudad_list)
+        print (mat_ciudades)
+        
+        
+
+        
+
+
+
+
+
 
     elif opc == 2:
         pass
+
+   
 
     else:
         si_no = input("""\n¿Está seguro que desea salir? 
@@ -62,4 +122,4 @@ while True:
             break
         else:
             input ("\nPresione cualquier tecla para volver al menú... ")
-            pass
+            
