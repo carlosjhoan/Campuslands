99fa72a96eb93c58033c2aa2d412d41429a85b65
carlosjhoan <43355110+carlosjhoan@users.noreply.github.com>
2023-10-11 02:17:25 +0000

    agregando contenido .read, .readline, .readlines

--- Archivos/lectura_archivo.py
+++ Archivos/lectura_archivo.py
@@ -1,7 +1,14 @@
-archivo = open("Archivos", "r")
+archivo = open("Archivos/nombres.txt", "r")
 
 
 texto = archivo.read()
-print (texto)
+print ("\ntexto:\n",texto) #Me ubica el descriptor al final
+
+archivo.seek(0) #Se ubica en la posición 0
+texto_2 = archivo.readline() #Es la que más se utiliza para no correr el riesgo sin quedarnos sin memoria
+print ("\ntexto_2:\n",texto_2)
+
+texto_3 = archivo.readlines()
+print ("\ntexto3:\n",texto_3)
 
 archivo.close()
