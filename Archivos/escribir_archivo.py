a605f855229205be43aab744169a166acb33195a
carlosjhoan <43355110+carlosjhoan@users.noreply.github.com>
2023-10-11 02:31:13 +0000

    seguimos aprendiendo del modo escritura

--- Archivos/escribir_archivo.py
+++ Archivos/escribir_archivo.py
@@ -1,5 +1,7 @@
 archivo = open("Archivos/salas.txt", "w")
 
-archivo.write("Sputnik")
+archivo.write("Sputnik\n\t")
+archivo.write("Apolo\n")
+archivo.writelines(["Houston\n", "Artemis\n"])
 print("\nArchivo: ", archivo)
 archivo. close()
